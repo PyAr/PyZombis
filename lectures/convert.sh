15b230de7b28af9c6524784d7bf7270cc1c4dc5e
Mariano Reingart <reingart@gmail.com>
2015-10-15 15:28:00 -0300

    incluir titulo del PPTX en el RST

--- lectures/convert.sh
+++ lectures/convert.sh
@@ -6,10 +6,15 @@ for PPT in TWP*.pptx; do
     XHTML="${PPT%.pptx}.xhtml"
     HTML="${PPT:0:5}.html"
 
+    TITLE=${PPT%.pptx}
+    TITLE=${TITLE:6:99}
+    LEN=${#TITLE}
+
+    echo "Processing $TITLE"
     loffice --headless --convert-to xhtml "$PPT" 
     echo "Converting $RST"
-    echo "Python para zombis" > "$RST"
-    echo "==================" >> "$RST"
+    echo "$TITLE" > "$RST"
+    printf '%*s\n' $LEN "" | tr ' ' '=' >> "$RST"
     echo  >> "$RST"
     python html2rest.py  "$XHTML" >> "$RST"
     echo "Generating presentation in $HTML"
