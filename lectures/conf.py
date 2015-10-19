458b8528cccb7c9e009eb793f22bc8b4e2e5793d
Mariano Reingart <reingart@gmail.com>
2015-10-18 22:19:17 -0300

    Agrego estilo css personalizado (ajustar margen títulos)

--- lectures/conf.py
+++ lectures/conf.py
@@ -139,6 +139,8 @@ html_theme = 'alabaster'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+slide_theme_options = {'custom_css': 'custom.css'}
+
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
 # directly to the root of the documentation.
