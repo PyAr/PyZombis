b95fcacab345280767b66b537636e027d585615c
Mariano Reingart <reingart@gmail.com>
2021-05-25 23:23:46 -0300

    Record test videos & upload artifact

--- conftest.py
+++ conftest.py
@@ -1,33 +1,32 @@
-import asyncio
-from asyncio import AbstractEventLoop
-from typing import Any, Callable, Dict, Generator, List, Optional
+# Playwright video recording original implementation:
+#   https://github.com/alethcoe/playwright_recording_pytest
+
+from typing import Any, Dict, Generator, List
 
 import pytest
 from playwright.sync_api import (
     Browser,
     BrowserContext,
     Page,
-    Playwright,
-    sync_playwright,
 )
 
 import os
 import shutil
+
 
 @pytest.fixture(scope="session")
 def browser_context_args(browser_context_args, video_path):
     return {
         **browser_context_args,
         "record_video_dir": video_path,
-        "viewport": {
-            "width": 1920,
-            "height": 1080
-        }
+        "viewport": {"width": 1920, "height": 1080},
     }
+
 
 @pytest.fixture(scope="session")
 def video_path():
     return "./videos"
+
 
 def pytest_sessionstart(session):
     """
@@ -41,6 +40,7 @@ def pytest_sessionstart(session):
             except OSError:
                 os.remove(filepath)
 
+
 @pytest.fixture
 def page(context: BrowserContext, base_url: str) -> Generator[Page, None, None]:
     page = context.new_page()
@@ -50,11 +50,12 @@ def page(context: BrowserContext, base_url: str) -> Generator[Page, None, None]:
     )
     yield page
 
-    #save off the test unique id
+    # save off the test unique id
     current_path_name = context.pages[0].video.path().name
     BrowserContext.current_video_name = current_path_name
 
     page.close()
+
 
 @pytest.fixture
 def context(
@@ -65,12 +66,15 @@ def context(
     yield context
     current_video_name = context.current_video_name
     current_video_path = os.path.join(video_path, current_video_name)
-    updated_video_path = os.path.join(video_path, f'{request.node.originalname}_{browser_name}.webm')
+    updated_video_path = os.path.join(
+        video_path, f"{request.node.originalname}_{browser_name}.webm"
+    )
     context.close()
     os.rename(current_video_path, updated_video_path)
     if request.session.testsfailed == current_failed_tests:
-        #test should have been successful no real reason to keep it
+        # test should have been successful no real reason to keep it
         os.remove(updated_video_path)
+
 
 def _handle_page_goto(
     page: Page, args: List[Any], kwargs: Dict[str, Any], base_url: str
