25eb3a5ea366d3179018d9b3dea10b5887821dc5
reingart <reingart@users.noreply.github.com>
2021-05-29 17:41:34 +0000

    deploy: 15db78af355e470296deeb049e7c6bbb71e3d90f

---
+++ 79/_static/documentation_options.js
@@ -0,0 +1,12 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: '96d7dc9cd47d5d5f915432cd4505e101c36f6158-tasklist',
+    LANGUAGE: 'None',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false
+};
