7557fd613a2dae417d4cac8ce39f67763701c2b7
angelasofiaremolinagutierrez <angela.remolina.2019@upb.edu.co>
2021-06-07 18:09:32 -0500

    General navigation test with playwright

---
+++ tests/test_navigation.py
@@ -0,0 +1,13 @@
+def test_right_arrows(page):
+    page.goto("index.html")
+    while(True):
+        #keeps going to the next page until there is no right arrow 
+        right_arrow = page.query_selector("//*[@id='relations-next']/a")
+        if(right_arrow):
+            page.click("//*[@id='relations-next']/a")
+            page.wait_for_load_state()
+        else: 
+            break
+
+#TODO make a similar test but going from de last page to the previous one until it gets to the first one
+            
