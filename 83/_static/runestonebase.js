d7ca9fc00679185b0db2e31e9a95ca85403912c0
LeoCumpli21 <LeoCumpli21@users.noreply.github.com>
2021-06-08 23:11:17 +0000

    deploy: 62c86ca4eed2a0aa6a0e324b35233729b602c4e5

---
+++ 83/_static/runestonebase.js
@@ -0,0 +1,348 @@
+/* ********************************
+ * |docname| - Runestone Base Class
+ * ********************************
+ * All runestone components should inherit from RunestoneBase. In addition all runestone components should do the following things:
+ *
+ * 1.   Ensure that they are wrapped in a div with the class runestone
+ * 2.   Write their source AND their generated html to the database if the database is configured
+ * 3.   Properly save and restore their answers using the checkServer mechanism in this base class. Each component must provide an implementation of:
+ *
+ *      -    checkLocalStorage
+ *      -    setLocalStorage
+ *      -    restoreAnswers
+ *      -    disableInteraction
+ *
+ * 4.   provide a Selenium based unit test
+ */
+
+import { pageProgressTracker } from "./bookfuncs.js";
+//import "./../styles/runestone-custom-sphinx-bootstrap.css";
+
+export default class RunestoneBase {
+    constructor(opts) {
+        this.component_ready_promise = new Promise(resolve => this._component_ready_resolve_fn = resolve)
+        this.optional = false;
+        if (opts) {
+            this.sid = opts.sid;
+            this.graderactive = opts.graderactive;
+            this.showfeedback = true;
+            if (opts.timed) {
+                this.isTimed = true;
+            }
+            if (opts.enforceDeadline) {
+                this.deadline = opts.deadline;
+            }
+            if ($(opts.orig).data("optional")) {
+                this.optional = true;
+            } else {
+                this.optional = false;
+            }
+            if (opts.selector_id) {
+                this.selector_id = opts.selector_id;
+            }
+            if (typeof opts.assessmentTaken !== "undefined") {
+                this.assessmentTaken = opts.assessmentTaken;
+            } else {
+                // default to true as this opt is only provided from a timedAssessment
+                this.assessmentTaken = true;
+            }
+            // This is for the selectquestion points
+            // If a selectquestion is part of a timed exam it will get
+            // the timedWrapper options.
+            if (typeof opts.timedWrapper !== "undefined") {
+                this.timedWrapper = opts.timedWrapper;
+            } else {
+                // However sometimes selectquestions
+                // are used in regular assignments.  The hacky way to detect this
+                // is to look for doAssignment in the URL and then grab
+                // the assignment name from the heading.
+                if (location.href.indexOf("doAssignment") >= 0) {
+                    this.timedWrapper = $("h1#assignment_name").text()
+                } else {
+                    this.timedWrapper = null;
+                }
+            }
+            if ($(opts.orig).data("question_label")) {
+                this.question_label = $(opts.orig).data("question_label");
+            }
+        }
+        this.jsonHeaders = new Headers({
+            "Content-type": "application/json; charset=utf-8",
+            Accept: "application/json",
+        });
+    }
+
+    // .. _logBookEvent:
+    //
+    // logBookEvent
+    // ------------
+    // This function sends the provided ``eventInfo`` to the `hsblog endpoint` of the server. Awaiting this function returns either ``undefined`` (if Runestone services are not available) or the data returned by the server as a JavaScript object (already JSON-decoded).
+    async logBookEvent(eventInfo) {
+        if (this.graderactive) {
+            return;
+        }
+        let post_return;
+        eventInfo.course = eBookConfig.course;
+        eventInfo.clientLoginStatus = eBookConfig.isLoggedIn;
+        eventInfo.timezoneoffset = new Date().getTimezoneOffset() / 60;
+        if (this.percent) {
+            eventInfo.percent = this.percent;
+        }
+        if (eBookConfig.useRunestoneServices && eBookConfig.logLevel > 0) {
+            let request = new Request(eBookConfig.ajaxURL + "hsblog", {
+                method: "POST",
+                headers: this.jsonHeaders,
+                body: JSON.stringify(eventInfo),
+            });
+            try {
+                let response = await fetch(request);
+                if (!response.ok) {
+                    throw new Error("Failed to save the log entry");
+                }
+                post_return = response.json();
+            } catch (e) {
+                if (this.isTimed) {
+                    alert(`Error: Your action was not saved! The error was ${e}`);
+                }
+                console.log(`Error: ${e}`);
+            }
+        }
+        if (!this.isTimed || eBookConfig.debug) {
+            console.log("logging event " + JSON.stringify(eventInfo));
+        }
+        if (
+            typeof pageProgressTracker.updateProgress === "function" &&
+            eventInfo.act != "edit" &&
+            this.optional == false
+        ) {
+            pageProgressTracker.updateProgress(eventInfo.div_id);
+        }
+        return post_return;
+    }
+
+    // .. _logRunEvent:
+    //
+    // logRunEvent
+    // -----------
+    // This function sends the provided ``eventInfo`` to the `runlog endpoint`. When awaited, this function returns the data (decoded from JSON) the server sent back.
+    async logRunEvent(eventInfo) {
+        let post_promise = "done";
+        if (this.graderactive) {
+            return;
+        }
+        eventInfo.course = eBookConfig.course;
+        eventInfo.clientLoginStatus = eBookConfig.isLoggedIn;
+        eventInfo.timezoneoffset = new Date().getTimezoneOffset() / 60;
+        if (this.forceSave || "to_save" in eventInfo === false) {
+            eventInfo.save_code = "True";
+        }
+        if (eBookConfig.useRunestoneServices && eBookConfig.logLevel > 0) {
+            let request = new Request(eBookConfig.ajaxURL + "runlog.json", {
+                method: "POST",
+                headers: this.jsonHeaders,
+                body: JSON.stringify(eventInfo),
+            });
+            let response = await fetch(request);
+            if (!response.ok) {
+                throw new Error("Failed to log the run");
+            }
+            post_promise = await response.json();
+        }
+        if (!this.isTimed || eBookConfig.debug) {
+            console.log("running " + JSON.stringify(eventInfo));
+        }
+        if (
+            typeof pageProgressTracker.updateProgress === "function" &&
+            this.optional == false
+        ) {
+            pageProgressTracker.updateProgress(eventInfo.div_id);
+        }
+        return post_promise;
+    }
+    /* Checking/loading from storage
+    **WARNING:**  DO NOT `await` this function!
+    This function, although async, does not explicitly resolve its promise by returning a value.  The reason for this is because it is called by the constructor for nearly every component.  In Javascript constructors cannot be async!
+
+    One of the recommended ways to handle the async requirements from within a constructor is to use an attribute as a promise and resolve that attribute at the appropriate time.
+    */
+    async checkServer(
+        // A string specifying the event name to use for querying the :ref:`getAssessResults` endpoint.
+        eventInfo,
+        // If true, this function will invoke ``indicate_component_ready()`` just before it returns. This is provided since most components are ready after this function completes its work.
+        //
+        // TODO: This defaults to false, to avoid causing problems with any components that haven't been updated and tested. After all Runestone components have been updated, default this to true and remove the extra parameter from most calls to this function.
+        will_be_ready = false
+    ) {
+        // Check if the server has stored answer
+        let self = this;
+        this.checkServerComplete = new Promise(function (resolve, reject) {
+            self.csresolver = resolve;
+        });
+        if (this.useRunestoneServices || this.graderactive) {
+            let data = {};
+            data.div_id = this.divid;
+            data.course = eBookConfig.course;
+            data.event = eventInfo;
+            if (this.graderactive && this.deadline) {
+                data.deadline = this.deadline;
+                data.rawdeadline = this.rawdeadline;
+                data.tzoff = this.tzoff;
+            }
+            if (this.sid) {
+                data.sid = this.sid;
+            }
+            if (!eBookConfig.practice_mode && this.assessmentTaken) {
+                let request = new Request(
+                    eBookConfig.ajaxURL + "getAssessResults",
+                    {
+                        method: "POST",
+                        body: JSON.stringify(data),
+                        headers: this.jsonHeaders,
+                    }
+                );
+                try {
+                    let response = await fetch(request);
+                    data = await response.json();
+                    this.repopulateFromStorage(data);
+                    this.csresolver("server");
+                } catch (err) {
+                    try {
+                        this.checkLocalStorage();
+                    } catch (err) {
+                        console.log(err);
+                    }
+                }
+            } else {
+                this.loadData({});
+                this.csresolver("not taken");
+            }
+        } else {
+            this.checkLocalStorage(); // just go right to local storage
+            this.csresolver("local");
+        }
+
+        if (will_be_ready) {
+            this.indicate_component_ready();
+        }
+    }
+
+    // This method assumes that ``this.componentDiv`` refers to the ``div`` containing the component, and that this component's ID is set.
+    indicate_component_ready() {
+        // Add a class to indicate the component is now ready.
+        this.containerDiv.classList.add("runestone-component-ready");
+        // Resolve the ``this.component_ready_promise``.
+        this._component_ready_resolve_fn();
+    }
+
+    loadData(data) {
+        // for most classes, loadData doesn't do anything. But for Parsons, and perhaps others in the future,
+        // initialization can happen even when there's no history to be loaded
+        return null;
+    }
+
+    /**
+     * repopulateFromStorage is called after a successful API call is made to ``getAssessResults`` in
+     * the checkServer method in this class
+     *
+     * ``restoreAnswers,`` ``setLocalStorage`` and ``checkLocalStorage`` are defined in the child classes.
+     *
+     * @param {*} data - a JSON object representing the data needed to restore a previous answer for a component
+     * @param {*} status - the http status
+     * @param {*} whatever - ignored
+     */
+    repopulateFromStorage(data) {
+        // decide whether to use the server's answer (if there is one) or to load from storage
+        if (data !== null && this.shouldUseServer(data)) {
+            this.restoreAnswers(data);
+            this.setLocalStorage(data);
+        } else {
+            this.checkLocalStorage();
+        }
+    }
+    shouldUseServer(data) {
+        // returns true if server data is more recent than local storage or if server storage is correct
+        if (
+            data.correct === "T" ||
+            localStorage.length === 0 ||
+            this.graderactive === true ||
+            this.isTimed
+        ) {
+            return true;
+        }
+        let ex = localStorage.getItem(this.localStorageKey());
+        if (ex === null) {
+            return true;
+        }
+        let storedData;
+        try {
+            storedData = JSON.parse(ex);
+        } catch (err) {
+            // error while parsing; likely due to bad value stored in storage
+            console.log(err.message);
+            localStorage.removeItem(this.localStorageKey());
+            // definitely don't want to use local storage here
+            return true;
+        }
+        if (data.answer == storedData.answer) return true;
+        let storageDate = new Date(storedData.timestamp);
+        let serverDate = new Date(data.timestamp);
+        return serverDate >= storageDate;
+    }
+    // Return the key which to be used when accessing local storage.
+    localStorageKey() {
+        return (
+            eBookConfig.email +
+            ":" +
+            eBookConfig.course +
+            ":" +
+            this.divid +
+            "-given"
+        );
+    }
+    addCaption(elType) {
+        //someElement.parentNode.insertBefore(newElement, someElement.nextSibling);
+        if (!this.isTimed) {
+            var capDiv = document.createElement("p");
+            if (this.question_label) {
+                this.caption = `Activity: ${this.question_label} ${this.caption}  <span class="runestone_caption_divid">(${this.divid})</span>`;
+                $(capDiv).html(this.caption);
+                $(capDiv).addClass(`${elType}_caption`);
+            } else {
+                $(capDiv).html(this.caption + " (" + this.divid + ")");
+                $(capDiv).addClass(`${elType}_caption`);
+                $(capDiv).addClass(`${elType}_caption_text`);
+            }
+            this.capDiv = capDiv;
+            //this.outerDiv.parentNode.insertBefore(capDiv, this.outerDiv.nextSibling);
+            this.containerDiv.appendChild(capDiv);
+        }
+    }
+
+    hasUserActivity() {
+        return this.isAnswered;
+    }
+
+    checkCurrentAnswer() {
+        console.log(
+            "Each component should provide an implementation of checkCurrentAnswer"
+        );
+    }
+
+    async logCurrentAnswer() {
+        console.log(
+            "Each component should provide an implementation of logCurrentAnswer"
+        );
+    }
+    renderFeedback() {
+        console.log(
+            "Each component should provide an implementation of renderFeedback"
+        );
+    }
+    disableInteraction() {
+        console.log(
+            "Each component should provide an implementation of disableInteraction"
+        );
+    }
+}
+
+window.RunestoneBase = RunestoneBase;
