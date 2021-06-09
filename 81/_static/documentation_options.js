499c77b0d7c9378a43d9f1927f8f5227ca96c92c
LeoCumpli21 <LeoCumpli21@users.noreply.github.com>
2021-06-09 02:09:47 +0000

    deploy: 62c86ca4eed2a0aa6a0e324b35233729b602c4e5

--- 81/_static/documentation_options.js
+++ 81/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'f63eabf0cd56ce8d546e4f9c740242f65a073bae-fixQuizzes',
+    VERSION: '9453eb5a5fa7f16ce761ea73894724bb82204393-fixQuizzes',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
