b2dd53295e2d083ae2926e49fe20b32741da0983
Ybrahin Martinez <ybrahimmartinez09@gmail.com>
2021-06-12 22:30:20 -0400

    Add the quiz10 and test

---
+++ tests/test_quiz10.py
@@ -0,0 +1,18 @@
+def test_quiz10_11_good(page):
+    # Go to the Quiz 10 page
+    page.goto("quiz/Quiz10.html")
+    page.wait_for_load_state()
+
+    page.click("text=def tiene_2(numeros):")
+
+    page.press("text=def tiene_2(numeros):", "ArrowDown")
+    page.press("text=def tiene_2(numeros):", "Tab")
+
+    page.type("text=def tiene_2(numeros):", "return '2, 2' in str(numeros)")
+
+    page.click("#q10_11 >> *css=button >> text=Run")
+
+    page.hover("#q10_11 >> text=You passed:")
+    page.press("text=def tiene_2(numeros):", "ArrowDown")
+
+    assert page.inner_text("#q10_11 >> text=You passed:") == "You passed: 100.0% of the tests"
