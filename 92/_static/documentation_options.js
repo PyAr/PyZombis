2e9d0651eb7342314aaf5e2c3238906fa1d4438e
y-martinez <y-martinez@users.noreply.github.com>
2021-06-14 00:17:08 +0000

    deploy: 9ed3842b742c6de6a43a2878d62f6773b9c5a504

--- 92/_static/documentation_options.js
+++ 92/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2ae685ee39c0c3eee89f8c3e3fcf53a109121794-15_quiz10',
+    VERSION: '6701ef6442ee3499aa4df93cbba803a84b8bb522-15_quiz10',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
