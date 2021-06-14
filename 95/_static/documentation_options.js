abc38d058dde7c83a53aef30ac71fff4f5bbc3ae
LeoCumpli21 <LeoCumpli21@users.noreply.github.com>
2021-06-14 19:17:05 +0000

    deploy: dfc341bf287a8923163ea0ce653dbb49df69b56a

---
+++ 95/_static/documentation_options.js
@@ -0,0 +1,12 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: '00edeca5f78b21fe51c394532d940c78565b0f24-FixingQuizzes',
+    LANGUAGE: 'None',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false
+};
