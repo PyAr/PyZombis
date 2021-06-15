b0ab3e2000ec35870a4e021d5576acd113179b80
Ybrahin Martinez <ybrahimmartinez09@gmail.com>
2021-06-15 00:50:43 -0400

    Improve the quiz extra 2 and test

---
+++ tests/test_quizExtra2.py
@@ -0,0 +1,16 @@
+def test_quizExtra_3_bad(page):
+    # Go to Quiz Extras 2 page
+    page.goto("quiz/QuizExtras2.html")
+
+    # Complete the exercise
+    page.click("text=def calcular_pi(n):")
+    page.press("text=def calcular_pi(n):", "ArrowDown")
+    page.press("text=def calcular_pi(n):", "Tab")
+    page.type("text=def calcular_pi(n):", "return 2")
+
+    # Click button:has-text("Run")
+    page.click("#qExtra_3 >> *css=button >> text=Run")
+
+    # Test it passed all unit tests
+    page.hover("#qExtra_3 >> text=You passed:")
+    assert page.inner_text("#qExtra_3 >> text=You passed:") == "You passed: 0.0% of the tests"
