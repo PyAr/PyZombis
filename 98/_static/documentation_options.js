e8542cc6a3ec375cca71c43fbab219ebfc3c6956
y-martinez <y-martinez@users.noreply.github.com>
2021-06-15 04:58:37 +0000

    deploy: dfc341bf287a8923163ea0ce653dbb49df69b56a

---
+++ 98/_static/documentation_options.js
@@ -0,0 +1,12 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: 'b0ab3e2000ec35870a4e021d5576acd113179b80-18_quiz_extra_2',
+    LANGUAGE: 'None',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false
+};
