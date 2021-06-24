762bb4f7c8817fcf0d6fb3168d2b305d2a418add
y-martinez <y-martinez@users.noreply.github.com>
2021-06-24 20:32:28 +0000

    deploy: 1b2c6d01de8307870a9eb8e8cbdf13b1046536cc

---
+++ 110/_static/documentation_options.js
@@ -0,0 +1,12 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: 'da9d1fd8c2374011a6f4bb11d4901ac10fc68f92-16_quiz12',
+    LANGUAGE: 'None',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false
+};
