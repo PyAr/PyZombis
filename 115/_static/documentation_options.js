81ad358250db549662a803ee4f5d7c95d86cf80b
y-martinez <y-martinez@users.noreply.github.com>
2021-06-26 03:05:03 +0000

    deploy: 6756275f9bf21c15681cab783c38f23e5fe811e9

---
+++ 115/_static/documentation_options.js
@@ -0,0 +1,12 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: '3706fc23a17cdec17f65fac69a81d29779ccd92f-23_update_lesson_twp33',
+    LANGUAGE: 'None',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false
+};
