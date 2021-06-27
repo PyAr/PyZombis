46c1ae9fccab794ea5bb3fb5bae5c4fcb4c1ecce
y-martinez <y-martinez@users.noreply.github.com>
2021-06-27 02:14:03 +0000

    deploy: 6756275f9bf21c15681cab783c38f23e5fe811e9

---
+++ 116/_static/documentation_options.js
@@ -0,0 +1,12 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: '20529a22eca80bc8d167f851c40d50b486f599a3-24_refactor_twp37',
+    LANGUAGE: 'None',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false
+};
