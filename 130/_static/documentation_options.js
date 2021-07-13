4dd96c8b9e3b51d7ed2aeaa9d5bde03725f28e40
angelasofiaremolinagutierrez <angelasofiaremolinagutierrez@users.noreply.github.com>
2021-07-13 22:08:27 +0000

    deploy: d76106dfe075404909bc6745bba01aae9ce9a18d

---
+++ 130/_static/documentation_options.js
@@ -0,0 +1,12 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: 'eef2128b7ad881c457ca0e079c12d94ae2ea5e96-intro-video',
+    LANGUAGE: 'None',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false
+};
