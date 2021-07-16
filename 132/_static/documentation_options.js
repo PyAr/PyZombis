95228334be858753a721491502ea84c510370d21
angelasofiaremolinagutierrez <angelasofiaremolinagutierrez@users.noreply.github.com>
2021-07-16 06:35:56 +0000

    deploy: d76106dfe075404909bc6745bba01aae9ce9a18d

--- 132/_static/documentation_options.js
+++ 132/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'ee5f94b7653130f31e1c2a9d53ac974cf654ef10-toctree',
+    VERSION: '27651ebae854bc23a4583a973522f49503d5036c-toctree',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
