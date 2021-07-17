3c723509a716aefd31eecbe0a7114fe2caec705f
LeoCumpli21 <LeoCumpli21@users.noreply.github.com>
2021-07-17 18:46:56 +0000

    deploy: 3bb6c63b1cc9534ee5c55f8d0c1fcb2539cfc1fe

---
+++ 133/_static/documentation_options.js
@@ -0,0 +1,12 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: 'a0de155185286ccf7e5bff89811a87525c5dfde2-14_Pergamino',
+    LANGUAGE: 'None',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false
+};
