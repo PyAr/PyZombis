e9d2f5b761b049859ef820ad0e0ef8f4720dee91
angelasofiaremolinagutierrez <angela.remolina.2019@upb.edu.co>
2021-07-23 00:09:24 -0500

    Update test_TWP23.py

--- tests/test_TWP23.py
+++ tests/test_TWP23.py
@@ -1,18 +1,18 @@
 def test_TWP23_ac_5(page):
     # Go to TWP23 page
-    page.goto("lectures/TWP23.html")
+    page.goto("lectures/TWP23/TWP23_2.html")
 
     # Do the exercise
-    page.click("#ac_l23_5 >> text=aeiou")
-    page.press("#ac_l23_5 >> text=aeiou", "ArrowRight")
-    page.type("#ac_l23_5 >> text=aeiou", "éá")
-    page.press("#ac_l23_5 >> text=aeiéáou", "ArrowUp")
-    page.press("#ac_l23_5 >> text=aeiéáou", "ArrowLeft")
-    page.type("#ac_l23_5 >> text=aeiéáou", ".lower()")
+    page.click("#ac_l23_2b >> text=aeiou")
+    page.press("#ac_l23_2b >> text=aeiou", "ArrowRight")
+    page.type("#ac_l23_2b >> text=aeiou", "éá")
+    page.press("#ac_l23_2b >> text=aeiéáou", "ArrowUp")
+    page.press("#ac_l23_2b >> text=aeiéáou", "ArrowLeft")
+    page.type("#ac_l23_2b >> text=aeiéáou", ".lower()")
 
     # Run the exercise
-    page.click("#ac_l23_5 >> *css=button >> text=Run")
+    page.click("#ac_l23_2b >> *css=button >> text=Run")
 
     # Check whether it passed all unit tests
-    page.hover("#ac_l23_5 >> text=You passed:")
-    assert page.inner_text("#ac_l23_5 >> text=You passed:") == "You passed: 100.0% of the tests"
+    page.hover("#ac_l23_2b >> text=You passed:")
+    assert page.inner_text("#ac_l23_2b >> text=You passed:") == "You passed: 100.0% of the tests"
