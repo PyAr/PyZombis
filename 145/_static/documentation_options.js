432ce343672c4e64d9140a173dd362b5ac58441f
angelasofiaremolinagutierrez <angelasofiaremolinagutierrez@users.noreply.github.com>
2021-07-24 02:42:15 +0000

    deploy: 1b0bc1fe9ef120beb8d909e1267efcbb96f4f3a8

---
+++ 145/_static/documentation_options.js
@@ -0,0 +1,12 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: '706ecf43d99051abc8a2301b3b8c79e068725157-split-TWP33',
+    LANGUAGE: 'None',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false
+};
