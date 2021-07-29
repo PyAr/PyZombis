0300821a7c20f60b0aa3ed181c7d2061d568d529
NicolasSandoval <NicolasSandoval@users.noreply.github.com>
2021-07-29 01:15:15 +0000

    deploy: 00f99cc246242ffb88c25233b7b5093e3e82bf45

--- master/_sources/index.rst.txt
+++ master/_sources/index.rst.txt
@@ -42,7 +42,7 @@ Contenidos:
    lectures/TWP33/toctree
    lectures/TWP35/toctree
    lectures/TWP37/toctree
-   lectures/TWP38
+   lectures/TWP38/toctree
    lectures/TWP40/toctree
    lectures/TWP42
    lectures/TWP45
--- master/_static/documentation_options.js
+++ master/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '70ae33e650b0711b5549c190276889eaf6b7c8bd-master',
+    VERSION: '864e315c594bea452e0b31d18f3a3db2d2a19804-master',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
