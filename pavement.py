36432f67e1f8dad7dd7359ada0333bcb43d5b211
Ybrahin Martinez <ybrahimmartinez09@gmail.com>
2021-07-28 22:16:45 -0400

    Update the structure of the project

--- pavement.py
+++ pavement.py
@@ -1,65 +1,76 @@
-import paver
-from paver.easy import *
-import paver.setuputils
-paver.setuputils.install_distutils_tasks()
-import os, sys
-from runestone.server import get_dburl
-from sphinxcontrib import paverutils
+import os
+import sys
 import pkg_resources
 from socket import gethostname
 
+from paver.easy import options, Bunch
+import paver.setuputils
+
+from runestone import get_master_url
+from runestone import (
+    build,
+)  # NOQA: F401 -- build is called implicitly by the paver driver.
+from runestone.server import get_dburl
+
+paver.setuputils.install_distutils_tasks()
 sys.path.append(os.getcwd())
 
 # The project name, for use below.
-project_name = "PyZombis"
+project_name = os.path.basename(os.path.dirname(os.path.abspath(__file__)))
+
+master_url = ""
+if not master_url:
+    master_url = get_master_url()
+
 # The root directory for ``runestone serve``.
 serving_dir = "./build/" + project_name
 # The destination directory for ``runestone deploy``.
 dest = "../../static"
 
 master_app = "runestone"
-master_url = "http://127.0.0.1:8000"
-
 
 options(
-    sphinx = Bunch(docroot=".",),
-
-    build = Bunch(
-        builddir="./build/PyZombisDev",
+    sphinx=Bunch(
+        docroot=".",
+    ),
+    build=Bunch(
+        builddir=serving_dir,
         sourcedir="_sources",
-        outdir="./build/PyZombis",
+        outdir=serving_dir,
         confdir=".",
-        project_name = "PyZombisDev",
-        template_args={'course_id': 'PyZombisDev',
-                       'login_required':'false',
-                       'appname':master_app,
-                       'loglevel': 0,
-                       'course_url':master_url,
-                       'use_services': 'false',
-                       'python3': 'true',
-                       'dburl': '',
-                       'default_ac_lang': 'python',
-                       'basecourse': 'PyZombisDev',
-                       'jobe_server': 'http://jobe2.cosc.canterbury.ac.nz',
-                       'proxy_uri_runs': '/jobe/index.php/restapi/runs/',
-                       'proxy_uri_files': '/jobe/index.php/restapi/files/',
-                       'downloads_enabled': 'false',
-                       'enable_chatcodes': 'false',
-                       'allow_pairs': 'false'
-                        }
-    )
+        project_name="PyZombisDev",
+        template_args={
+            "login_required": "false",
+            "loglevel": 10,
+            "course_title": project_name,
+            "python3": "true",
+            "dburl": "postgresql://user:password@localhost/runestone",
+            "default_ac_lang": "python",
+            "downloads_enabled": "false",
+            "enable_chatcodes": "false",
+            "allow_pairs": "false",
+            "use_services": "true",
+            "basecourse": project_name,
+            "course_id": project_name,
+            "jobe_server": "http://jobe2.cosc.canterbury.ac.nz",
+            "proxy_uri_runs": "/jobe/index.php/restapi/runs/",
+            "proxy_uri_files": "/jobe/index.php/restapi/files/",
+            "appname": master_app,
+            "course_url": master_url,
+        },
+    ),
 )
 
 # if we are on runestone-deploy then use the proxy server not canterbury
-if gethostname() == 'runestone-deploy':
-    del options.build.template_args['jobe_server']
-    del options.build.template_args['proxy_uri_runs']
-    del options.build.template_args['proxy_uri_files']
+if gethostname() == "runestone-deploy":
+    del options.build.template_args["jobe_server"]
+    del options.build.template_args["proxy_uri_runs"]
+    del options.build.template_args["proxy_uri_files"]
 
 version = pkg_resources.require("runestone")[0].version
-options.build.template_args['runestone_version'] = version
+options.build.template_args["runestone_version"] = version
 
 # If DBURL is in the environment override dburl
-options.build.template_args['dburl'] = get_dburl(outer=locals())
+options.build.template_args["dburl"] = get_dburl(outer=locals())
 
 from runestone import build  # build is called implicitly by the paver driver.
