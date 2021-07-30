8992301826cec32e36689449e1cbd4f6e0349de6
angelasofiaremolinagutierrez <angelasofiaremolinagutierrez@users.noreply.github.com>
2021-07-30 22:30:36 +0000

    deploy: 30802a426b9422fe521080a5bed4c202e62cdd20

--- 154/_sources/lectures/TWP42/toctree.rst.txt
+++ 154/_sources/lectures/TWP42/toctree.rst.txt
@@ -1,5 +1,5 @@
 ==============
-Banco de Dados
+Bases de datos
 ==============
 
 
--- 154/_static/documentation_options.js
+++ 154/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '375deb9ea1f4f1d89480b660ab7eb8d9f749ab55-split-TWP42',
+    VERSION: 'ebd77873c3669c4f3cd7fd8048b95df53dfcdefd-split-TWP42',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
