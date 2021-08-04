5cbcee498edfcaa6ebb5859c04eb17f516040315
y-martinez <y-martinez@users.noreply.github.com>
2021-08-04 02:10:29 +0000

    deploy: c757b8e0411aa809171f706a8716ec3e8b52a2fc

--- 162/_static/documentation_options.js
+++ 162/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'cadada007b20dd4a1823b97855f6ee250ab36a02-new-templates-files',
+    VERSION: 'b565adf14b7d236e38a0036f3410c2c009c2a36d-new-templates-files',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
