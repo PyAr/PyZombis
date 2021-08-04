106bc4e007974d46fc641d47e0cd51cdea7e4493
angelasofiaremolinagutierrez <angelasofiaremolinagutierrez@users.noreply.github.com>
2021-08-04 05:31:30 +0000

    deploy: c757b8e0411aa809171f706a8716ec3e8b52a2fc

---
+++ 164/_sources/lectures/TWP17/toctree.rst.txt
@@ -0,0 +1,21 @@
+======
+Listas
+======
+
+
+.. image:: ../img/TWP10_001.jpeg
+    :height: 14.925cm
+    :width: 9.258cm
+    :align: center
+    :alt:
+
+
+.. toctree::
+   :caption: Contenido
+   :maxdepth: 1
+
+   TWP17_1.rst
+   TWP17_2.rst
+   TWP17_3.rst
+   TWP17_4.rst
+   TWP17_5.rst
---
+++ 164/_static/documentation_options.js
@@ -0,0 +1,12 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: 'f1513981c2f8af65f976bd9c16a0a026e6fda734-SplitTWP45',
+    LANGUAGE: 'None',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false
+};
