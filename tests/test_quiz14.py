0c64475b90cc3e82aecf44d3283648a1bcc7410a
Leonardo Cumplido <cumplileo@gmail.com>
2021-08-05 10:18:51 -0500

    Changed quizzes tests structure

--- tests/test_quiz14.py
+++ tests/test_quiz14.py
@@ -3,13 +3,13 @@ def test_quiz14_1(page):
     page.goto("quiz/Quiz14.html")
     page.wait_for_load_state()
 
+    # Do the exercise
     page.click("text=def remover_iguales(numeros):")
+    page.keyboard.press("ArrowDown")
+    page.keyboard.press("Tab")
+    page.keyboard.type("return sorted(list(set(numeros)))")
 
-    page.press("text=remover_iguales(numeros):", "ArrowDown")
-    page.press("text=remover_iguales(numeros):", "Tab")
-
-    page.type("text=remover_iguales(numeros):", "return sorted(list(set(numeros)))")
-
+    # Run the exercise and check it passed all unit tests
     page.click("#q14_1 >> *css=button >> text=Run")
 
     page.hover("#q14_1 >> text=You passed:")
