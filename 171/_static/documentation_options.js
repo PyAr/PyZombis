7a0ea13c907c697b912ee38e7d11e1067ece26ae
angelasofiaremolinagutierrez <angelasofiaremolinagutierrez@users.noreply.github.com>
2021-08-09 05:29:24 +0000

    deploy: c757b8e0411aa809171f706a8716ec3e8b52a2fc

--- 171/_static/documentation_options.js
+++ 171/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'e47363b796fff9c107fa46ff24070e4fb2526402-Refactor-bases-de-datos',
+    VERSION: 'e81916aa7524b82ac0ad41c8d669fe1a7a6e9022-Refactor-bases-de-datos',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
