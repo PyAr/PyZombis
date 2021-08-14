2ab4a41f58c82859147c54b677c7747fe88b3c4a
NicolasSandoval <NicolasSandoval@users.noreply.github.com>
2021-08-14 17:10:08 +0000

    deploy: eca1d9ecc0674d5b098794b91fac087ed7977bb0

--- master/_static/documentation_options.js
+++ master/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'e05400161e49dfc4fd38f91990949aaaa79f8126-master',
+    VERSION: 'eca1d9ecc0674d5b098794b91fac087ed7977bb0-master',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
