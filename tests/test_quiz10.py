dfe907e6f7776d473292a2095c099913469a37b3
angelasofiaremolinagutierrez <angela.remolina.2019@upb.edu.co>
2021-08-15 13:47:06 -0500

    test Q11->Q10

--- tests/test_quiz10.py
+++ tests/test_quiz10.py
@@ -1,6 +1,6 @@
-def test_quiz11_3(page):
-    # Go to Quiz 11 page
-    page.goto("quiz/Quiz11.html")
+def test_quiz10_3(page):
+    # Go to Quiz 10 page
+    page.goto("quiz/Quiz10.html")
 
     # Do the exercise
     page.click("text=Ejercicio 3")
@@ -10,7 +10,7 @@ def test_quiz11_3(page):
     page.keyboard.type("return s[0] + s[1:].replace(s[0], '*')")
 
     # Run the exercise and check it passed all tests
-    page.click("#q11_3 >> *css=button >> text=Run")
+    page.click("#q10_3 >> *css=button >> text=Run")
 
-    page.hover("#q11_3 >> text=You passed:")
-    assert page.inner_text("#q11_3 >> text=You passed:") == "You passed: 100.0% of the tests"
+    page.hover("#q10_3 >> text=You passed:")
+    assert page.inner_text("#q10_3 >> text=You passed:") == "You passed: 100.0% of the tests"
