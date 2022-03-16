4f1d85da75386560d8b2fa7d168bbab0aff617de
shivamshan <shivamshan@users.noreply.github.com>
2022-03-16 03:53:23 +0000

    deploy: f48f63bbf433392c8222c53d3a78d11f29a06b60

--- 201/_static/documentation_options.js
+++ 201/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'ad1a008265d2ae07546e2990a9801a189e1d24ab-Pygame_POC',
+    VERSION: 'b72d5fd815434795f8900d4477ddd1cc473a9731-Pygame_POC',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
