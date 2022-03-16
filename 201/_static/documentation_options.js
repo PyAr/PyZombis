c2e0808fc439f12c92ead168f7db56fbaddc05c4
shivamshan <shivamshan@users.noreply.github.com>
2022-03-16 00:02:37 +0000

    deploy: 5e9aa7a9cb4e38fc9a0b52f316f21d33b01d6d71

---
+++ 201/_static/documentation_options.js
@@ -0,0 +1,12 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: 'ad1a008265d2ae07546e2990a9801a189e1d24ab-Pygame_POC',
+    LANGUAGE: 'None',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false
+};
---
+++ 201/pygame/version.py
@@ -0,0 +1,33 @@
+# "pygame" - the python MOCKUP
+# Copyright (C) License GNU GPL
+# authors:
+# Pete Shinners (original software) pete@shinners.org
+# Thomas Iwaszko (feature replication & revamping) thomas@gaudia-tech.com
+
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Library General Public
+# License as published by the Free Software Foundation; either
+# version 2 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Library General Public License for more details.
+#
+# You should have received a copy of the GNU Library General Public
+# License along with this library; if not, write to the Free
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+
+"""
+The version information is stored in the pygame mockup module as
+'pygame.ver'. Keeping the version information also available in a
+separate module allows you to test the pygame version without
+importing the main pygame mockup module.
+
+The version information should follow real pygame library's
+versioning rules, since the moment compatibility was tested.
+"""
+
+ver = '2.0.1-PY-MOCKUP'
+vernum = 2, 0, 1
