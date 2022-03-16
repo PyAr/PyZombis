5ef870cabde233401f985a16108b2a46881ef8ca
shivamshan <shivamshan@users.noreply.github.com>
2022-03-16 22:58:38 +0000

    deploy: f48f63bbf433392c8222c53d3a78d11f29a06b60

---
+++ 206/_static/documentation_options.js
@@ -0,0 +1,12 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: '04ed53922ce17c8a17393e64256e7d013ff9eaf9-TWP35_failing_proxy',
+    LANGUAGE: 'None',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false
+};
