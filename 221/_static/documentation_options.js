851083a2c4b8315630fb9ba513f8301fbcb5f05d
shivamshan <shivamshan@users.noreply.github.com>
2022-03-24 20:36:25 +0000

    deploy: 3036067d5a662b05cf6e4a32db0345a5a721e0f9

---
+++ 221/_static/documentation_options.js
@@ -0,0 +1,12 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: '0854e7008cf9dd2e220824404a1eae8ab4fc0001-E2E-Tests-TWP33',
+    LANGUAGE: 'None',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false
+};
