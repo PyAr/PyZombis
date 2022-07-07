41833a5656544f35e524eece5fc507eaf8f618b0
shivamshan <shivamshan@users.noreply.github.com>
2022-07-07 14:45:45 +0000

    deploy: 8153ec6c486c215ab7eca757deea04049d042067

---
+++ 223/_static/documentation_options.js
@@ -0,0 +1,12 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: '45ddad9fe49c6172498c67224f8314f01388b3a8-Refactor-TWP42_2--Conceptos-básicos-de-SQL',
+    LANGUAGE: 'None',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false
+};
