dfc7e5947088545d1a29df3e9243c45f05af41b4
shivamshan <shivamshan@users.noreply.github.com>
2022-07-16 20:21:52 +0000

    deploy: 8153ec6c486c215ab7eca757deea04049d042067

---
+++ 224/_static/documentation_options.js
@@ -0,0 +1,12 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: 'f3b8dd7962aab131725815f08b7238766575f5fb-TWP42_2---Más-conceptos-de-SQL',
+    LANGUAGE: 'None',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false
+};
