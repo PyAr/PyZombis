784516541e6e0b6e521d0e148c9e4a5bdbd9d0be
shivamshan <shivamshan@users.noreply.github.com>
2022-09-10 22:29:42 +0000

    deploy: 3dc56cea96c1e6f63af565e870a406adf38d7687

---
+++ 226/_static/documentation_options.js
@@ -0,0 +1,12 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: '4365040f2ab355f78360fe01569d25c4c7c33e82-Pygame_E2E_Tests',
+    LANGUAGE: 'None',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false
+};
