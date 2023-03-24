0e4a3a3ee247109de4ced4352897c0eea6ee1507
ivanuxowo <ivanuxowo@users.noreply.github.com>
2023-03-24 18:03:12 +0000

    deploy: 35c1d035b113bc6684ea8002c1e70ac9a3dea6f7

---
+++ 252/_static/documentation_options.js
@@ -0,0 +1,12 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: 'cccb33fd55e73baed7c60ebafe4423800ee1e430-patch-1',
+    LANGUAGE: 'None',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false
+};
