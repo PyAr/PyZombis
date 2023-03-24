d78677a81916fc3d8aba1712f13a48a4f33a4803
sujith-rek <sujith-rek@users.noreply.github.com>
2023-03-24 20:58:00 +0000

    deploy: 35c1d035b113bc6684ea8002c1e70ac9a3dea6f7

---
+++ 254/_static/documentation_options.js
@@ -0,0 +1,12 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: 'd4db8c84fcb687b4502e925ad4fec1dce827ba07-feat/codelens',
+    LANGUAGE: 'None',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false
+};
