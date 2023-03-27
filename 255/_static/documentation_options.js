4166f24fb0e429358b77c10ce329fd39145417d8
sujith-rek <sujith-rek@users.noreply.github.com>
2023-03-27 11:06:35 +0000

    deploy: 35c1d035b113bc6684ea8002c1e70ac9a3dea6f7

---
+++ 255/_static/documentation_options.js
@@ -0,0 +1,12 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: '3e840e484efda64c077db188fc67f947a920045c-feat/pygame-challenge',
+    LANGUAGE: 'None',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false
+};
