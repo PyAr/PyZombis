3d67c49ce41ac5608f9772d89fcf973ef496a99f
Shivanshu97i <Shivanshu97i@users.noreply.github.com>
2023-04-24 19:09:55 +0000

    deploy: b2e84584f6c3e2db5fc659970ef07e8074670efb

---
+++ 264/_static/documentation_options.js
@@ -0,0 +1,12 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: '16b02daef0eddd550a50fa3bb982f91ecaad1512-Poc',
+    LANGUAGE: 'None',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false
+};
