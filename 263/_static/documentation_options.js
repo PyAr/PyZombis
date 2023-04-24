7957de2de14d0e3e9c6fae8d0a3c07d16fb980a5
evananyonga <evananyonga@users.noreply.github.com>
2023-04-24 18:21:31 +0000

    deploy: b2e84584f6c3e2db5fc659970ef07e8074670efb

---
+++ 263/_static/documentation_options.js
@@ -0,0 +1,12 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: '3334f31b51c7ee983b3c302b2d1c44337266c496-docker',
+    LANGUAGE: 'None',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false
+};
