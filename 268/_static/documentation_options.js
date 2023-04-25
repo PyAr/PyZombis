427f22fdc0415556d25d3c2a1ee943878856097a
mdelgadonyc <mdelgadonyc@users.noreply.github.com>
2023-04-25 01:51:12 +0000

    deploy: adc3d309bc014025de25e8096189bc53e179ac4e

---
+++ 268/_static/documentation_options.js
@@ -0,0 +1,12 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: '66c123025bccef77466518904434ec0e9a4b3671-main',
+    LANGUAGE: 'None',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false
+};
