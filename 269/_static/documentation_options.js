a655ce45b40cffae2c0bd9e4c647826f43f70b9e
reingart <reingart@users.noreply.github.com>
2023-04-25 17:28:45 +0000

    deploy: 9782af25915b6baa7ca648fe170a65158105f185

--- 269/_static/documentation_options.js
+++ 269/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0cf2b7389e79f735d4047a690b593ce16ebfca56-i18n-gettext',
+    VERSION: 'c79725802bc4dddd52243f015e513fc13b6917c0-i18n-gettext',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
