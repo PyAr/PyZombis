49689b20155d798b79c5b95895f9110a2baa5310
xPushpeshx <xPushpeshx@users.noreply.github.com>
2023-04-25 22:10:41 +0000

    deploy: 4949e50d321bfee1352c8408a3b6dfd86d213fa2

---
+++ 270/_static/documentation_options.js
@@ -0,0 +1,12 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: 'e39db837fe34606179df573d32cd60fd2b359849-TWP40',
+    LANGUAGE: 'None',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false
+};
