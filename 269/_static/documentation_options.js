931d727c8fe9ed739c2516ff0146768f9eebd47f
reingart <reingart@users.noreply.github.com>
2023-04-25 16:48:44 +0000

    deploy: 9782af25915b6baa7ca648fe170a65158105f185

---
+++ 269/_static/documentation_options.js
@@ -0,0 +1,12 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: 'da89cf0015090e2075692105a72f735d0dc39bc0-i18n-gettext',
+    LANGUAGE: 'None',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false
+};
