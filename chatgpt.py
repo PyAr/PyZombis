f744a2875f3733f7418e98150ab0ea850d07b1f0
evananyonga <evewish@gmail.com>
2023-04-25 18:28:43 -0600

    produce multiple english files

--- chatgpt.py
+++ chatgpt.py
@@ -1,21 +1,31 @@
 import os
 import sys
-import openai
+# import openai
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
+# openai.api_key = os.getenv("OPENAI_API_KEY")
 
-text = open(sys.argv[1]).read()
 
-print("translating: ")
-print(text)
+for root, dir, files in os.walk('_sources/lectures/TWP42'):
+  for filename in files:
+    full_path = os.path.join(root, filename)
+    text = open(full_path).read()
+    formatted_path = os.path.splitext(full_path)[0]
+    # print(formatted_path)
+    if not formatted_path.endswith('_en'):
+      formatted_path = formatted_path+'_en.rst'
+      translation = text # completion.choices[0].message.content
+      open(formatted_path, 'w').write(translation)
 
-completion = openai.ChatCompletion.create(
-  model="gpt-3.5-turbo",
-  messages=[
-    {"role": "user", "content": "Translate the following snippet in reStructuredText format with Python code from Spanish to English:\n\n" + text}
-  ]
-)
+# print("translating: ")
+# print(text)
 
-print(completion.choices[0].message.content)
+# completion = openai.ChatCompletion.create(
+#   model="gpt-3.5-turbo",
+#   messages=[
+#     {"role": "user", "content": "Translate the following snippet in reStructuredText format with Python code from Spanish to English:\n\n" + text}
+#   ]
+# )
 
-open(sys.argv[1]+"en.rst").write(completion.choices[0].message.content)
+# print(completion.choices[0].message.content)
+
+# open(sys.argv[1]+"en.rst").write(completion.choices[0].message.content)
