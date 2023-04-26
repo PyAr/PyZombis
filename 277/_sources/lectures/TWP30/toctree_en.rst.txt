83b4397a6ebba0606ac886bf4f44a8e26e54d217
mdelgadonyc <mdelgadonyc@users.noreply.github.com>
2023-04-26 22:27:59 +0000

    deploy: 47ac164729cfdfc202d7169a2fe95d2645d35e1d

---
+++ 277/_sources/lectures/TWP30/toctree_en.rst.txt
@@ -0,0 +1,24 @@
+==================
+Revisión general 1
+==================
+
+
+.. image:: ../img/TWP10_001.jpeg
+    :height: 14.925cm
+    :width: 9.258cm
+    :align: center
+    :alt:
+
+
+.. toctree::
+   :caption: Contenido
+   :maxdepth: 1
+   :numbered:
+
+   TWP30_1_en.rst
+   TWP30_2_en.rst
+   TWP30_3_en.rst
+   TWP30_4_en.rst
+   TWP30_5_en.rst
+   TWP30_6_en.rst
+   TWP30_7_en.rst
---
+++ 277/_static/documentation_options.js
@@ -0,0 +1,12 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: 'd7fe56232c1c5b68e7246277f95df39df5190b22-toc-english',
+    LANGUAGE: 'None',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false
+};
