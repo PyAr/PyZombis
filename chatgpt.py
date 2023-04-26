3690e2764284ab6b3c5f12ef351d84d24b4e1369
evananyonga <evewish@gmail.com>
2023-04-25 18:50:08 -0600

    Enable translation with openai

--- chatgpt.py
+++ chatgpt.py
@@ -1,8 +1,17 @@
 import os
 import sys
-# import openai
+import openai
 
-# openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.api_key = os.getenv("OPENAI_API_KEY")
+def chatgpt_translator(text):
+  completion = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[
+      {"role": "user", "content": "Translate the following snippet in reStructuredText format with Python code from Spanish to English:\n\n" + text}
+    ]
+  )
+
+  return completion.choices[0].message.content
 
 
 for root, dir, files in os.walk('_sources/lectures/TWP42'):
@@ -13,18 +22,13 @@ for root, dir, files in os.walk('_sources/lectures/TWP42'):
     # print(formatted_path)
     if not formatted_path.endswith('_en'):
       formatted_path = formatted_path+'_en.rst'
-      translation = text # completion.choices[0].message.content
+      translation = chatgpt_translator(text) 
       open(formatted_path, 'w').write(translation)
 
 # print("translating: ")
 # print(text)
 
-# completion = openai.ChatCompletion.create(
-#   model="gpt-3.5-turbo",
-#   messages=[
-#     {"role": "user", "content": "Translate the following snippet in reStructuredText format with Python code from Spanish to English:\n\n" + text}
-#   ]
-# )
+
 
 # print(completion.choices[0].message.content)
 
