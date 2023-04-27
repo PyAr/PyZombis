84d87c0e34856d23fcf5f182dabd78b9bfe74f71
mdelgadonyc <mdelgadonyc@users.noreply.github.com>
2023-04-27 19:23:36 +0000

    deploy: ab2ded6a8f5b2fa94484b578890aa6f410456c84

--- 279/_static/documentation_options.js
+++ 279/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'f6eae317ec45694c2d0e9b51b1e61bc9874d6159-toc-english2',
+    VERSION: '836e63579f9f6c4c1ef31e24c2fa6386aa938779-toc-english2',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
