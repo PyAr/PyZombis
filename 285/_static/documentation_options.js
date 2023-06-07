b9948a4258e653c6ae77e9507bf8296481c888e7
sujith-rek <sujith-rek@users.noreply.github.com>
2023-06-07 14:13:19 +0000

    deploy: 641a9828318b8a75387bb1b2c082769373136dbf

---
+++ 285/_static/documentation_options.js
@@ -0,0 +1,12 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: 'fed703f648e7d7b0fd2d0c2085a243916a18c6f3-feat/pymaze',
+    LANGUAGE: 'None',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false
+};
