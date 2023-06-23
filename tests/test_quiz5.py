3d0e93e9ccdd82e9e47d2c76f5b2fa9283dbdb49
sujith-rek <ps335@snu.edu.in>
2023-06-23 19:15:24 +0530

    add e2e tests for quiz 5

--- tests/test_quiz5.py
+++ tests/test_quiz5.py
@@ -18,4 +18,16 @@ def test_quiz5_10(page):
 def test_quiz5_10_en(page):
     page.goto("quiz/Quiz5_en.html")
 
-    
+    page.click("text=def es_numero_ponteironuloville(numero):")
+
+    page.keyboard.press("ArrowDown")
+    page.keyboard.press("Tab")
+
+    page.keyboard.type("return True")
+
+    page.click("#q5_10_en >> *css=button >> text=Run")
+
+    page.hover("#q5_10_en >> text=You passed:")
+    assert page.inner_text("#q5_10_en >> text=You passed:") == "You passed: 50.0% of the tests"
+
+
