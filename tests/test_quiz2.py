6cb1fae57f199220b673361d67fa46cb769afad4
sujith-rek <ps335@snu.edu.in>
2023-06-23 13:31:10 +0530

    feat: e2e test of quiz2

--- tests/test_quiz2.py
+++ tests/test_quiz2.py
@@ -13,3 +13,19 @@ def test_quiz2_2(page):
 
     page.hover("#q2_2 >> text=You passed:")
     assert page.inner_text("#q2_2 >> text=You passed:") == "You passed: 100.0% of the tests"
+
+def test_quiz2_2_en(page):
+    page.goto("quiz/Quiz2_en.html")
+
+    # Do the exercise
+    page.click("text=Exercise 2")
+    page.click("text=def es_bisiesto(anio):")
+    page.keyboard.press("ArrowDown")
+    page.keyboard.press("Tab")
+    page.keyboard.type("return anio % 4 == 0 and (anio % 100 != 0 or anio % 400 == 0)")
+
+    # Run the exercise and check it passed all unit tests
+    page.click("#q2_2_en >> *css=button >> text=Run")
+
+    page.hover("#q2_2_en >> text=You passed:")
+    assert page.inner_text("#q2_2_en >> text=You passed:") == "You passed: 100.0% of the tests"
