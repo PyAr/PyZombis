cf7f3cf7de69ff93ac3f4c1d8ad19a066b7f64ee
sujith-rek <ps335@snu.edu.in>
2023-06-23 23:40:17 +0530

    feat: e2e test of TWP42

--- tests/test_TWP42.py
+++ tests/test_TWP42.py
@@ -183,3 +183,184 @@ Name : Wild Honey
 '''
 
     assert output == res
+
+# def test_l42_1_en(page):
+#     page.goto("lectures/TWP42/TWP42_1_en.html")
+
+#     page.click("#ac_42_1_en >> *css=button >> text=Run")
+#     iframe = page.query_selector('//*[@id="ac_42_1_en"]/div/div[5]/iframe').content_frame()
+#     output = iframe.text_content('code')
+
+#     res = '''ID    : 101
+# Nombre : Johnny 'wave-boy' Jones
+# Pais  : USA
+# Media  : 8.32
+# Estilo : Fish
+# Edad  : 21
+
+# ID    : 102
+# Nombre : Juan Martino
+# Pais  : Spain
+# Media  : 9.01
+# Estilo : Gun
+# Edad  : 36
+
+# ID    : 103
+# Nombre : Joseph 'smitty' Smyth
+# Pais  : USA
+# Media  : 8.85
+# Estilo : Cruizer
+# Edad  : 18
+
+# ID    : 104
+# Nombre : Stacey O'Neill
+# Pais  : Ireland
+# Media  : 8.91
+# Estilo : Malibu
+# Edad  : 22
+
+# ID    : 105
+# Nombre : Aideen 'board babe' Wu
+# Pais  : Japan
+# Media  : 8.65
+# Estilo : Fish
+# Edad  : 24
+
+# ID    : 106
+# Nombre : Zack 'bonnie-lad' MacFadden
+# Pais  : Scotland
+# Media  : 7.82
+# Estilo : Thruster
+# Edad  : 26
+
+# ID    : 107
+# Nombre : Aaron Valentino
+# Pais  : Italy
+# Media  : 8.98
+# Estilo : Gun
+# Edad  : 19
+
+# '''
+
+#     assert output == res
+
+    
+# def test_l42_2(page):
+#     page.goto("lectures/TWP42/TWP42_2_en.html")
+
+#     page.click("text=4 >> pre[role=\"presentation\"]")
+#     page.fill("text=RunResetLoad History21 1from sqlite3 import connect2con = connect('students.bd')3cur = >> textarea", "cur.execute('''create table alumnos(login_id varchar(8),pass integer)''')")
+#     # Click button:has-text("Run")
+#     page.click("#ac_42_2_2_en >> *css=button >> text=Run")
+
+#     iframe = page.query_selector('//*[@id="ac_42_2_2_en"]/div/div[4]/iframe').content_frame()
+#     output = iframe.text_content('code')
+
+#     res = '''login    : masanori
+# ra : 421
+# login    : emengarda
+# ra : 666
+# '''
+
+#     assert output == res
+
+# def test_l42_3_1(page):
+    page.goto("lectures/TWP42/TWP42_3_en.html")
+    page.click("pre[role=\"presentation\"]:has-text(\"query = 'select * from Track limit 30;'\")")
+    page.keyboard.press("ArrowDown")
+    page.keyboard.press("Control+Shift+ArrowUp")
+    page.keyboard.press("Backspace")
+    page.fill("textarea", " 1'")
+    page.keyboard.press("Enter")
+    # Click button:has-text("Run")
+    page.click("#ac_42_3a_en >> *css=button >> text=Run")    
+    iframe = page.query_selector('//*[@id="ac_42_3a_en"]/div/div[4]/iframe').content_frame()
+    output = iframe.text_content('code')
+
+    res = '''TrackId : 1
+Name : For Those About To Rock (We Salute You)
+AlbumId : 1
+MediaTypeId : 1
+GenreId : 1
+Composer : Angus Young, Malcolm Young, Brian Johnson
+Milliseconds : 343719
+Bytes : 11170334
+UnitPrice : 0.99
+
+'''
+
+    assert output == res
+
+
+# def test_l42_3_2(page):
+#     page.goto("lectures/TWP42/TWP42_3_en.html")
+#     # page.click("#Sorting rows >> text=10​ >> pre[role=\"presentation\"]")
+#     # Press ArrowUp with modifiers
+#     page.click("text=3.3. Sorting rows¶")
+#     # Click text=The ORDER BY clause is used to sort a set of results from a query. SQLite stores
+#     page.click("text=The ORDER BY clause is used to sort a set of results from a query. SQLite stores")
+#     # Click #ac_42_3b_en >> text=Run
+   
+#     iframe = page.query_selector('//*[@id="ac_42_3b_en"]/div/div[5]/iframe')
+#     output = iframe.text_content('code')
+
+#     res = '''Name : É Uma Partida De Futebol
+# Milliseconds : 1071
+# AlbumId : 200
+
+# '''
+
+#     assert output == res
+
+
+def test_l42_3_3(page):
+    page.goto("lectures/TWP42/TWP42_3_en.html")
+    page.click("text=9​ >> pre[role=\"presentation\"]")
+    # Press ArrowUp with modifiers
+    page.press("text=3.4. Data filtering¶ We have already seen some ways of archiving data, e.g. >> textarea", "Control+Shift+ArrowUp")
+    # Press ArrowUp with modifiers
+    page.press("text=3.4. Data filtering¶ We have already seen some ways of archiving data, e.g. >> textarea", "Control+Shift+ArrowUp")
+    page.keyboard.press("Backspace")
+    page.fill("text=3.4. Data filtering¶ We have already seen some ways of archiving data, e.g. >> textarea", "query = '''select name, milliseconds, bytes, albumid from Track WHERE albumid=1 limit 1;'''")
+
+    page.keyboard.press("Enter")
+    # Click button:has-text("Run")
+    page.click("#ac_42_3c1_en >> *css=button >> text=Run")    
+    iframe = page.query_selector('//*[@id="ac_42_3c1_en"]/div/div[5]/iframe').content_frame()
+    output = iframe.text_content('code')
+
+    res = '''Name : For Those About To Rock (We Salute You)
+Milliseconds : 343719
+Bytes : 11170334
+AlbumId : 1
+
+'''
+
+    assert output == res
+
+
+def test_l42_3_4(page):
+    page.goto("lectures/TWP42/TWP42_3_en.html")
+    # Click button:has-text("Run")
+    page.click("#ac_42_3c4_en >> *css=button >> text=Run")    
+    iframe = page.query_selector('//*[@id="ac_42_3c4_en"]/div/div[5]/iframe').content_frame()
+    output = iframe.text_content('code')
+
+    res = '''TrackId : 1245
+Name : Wildest Dreams
+
+TrackId : 1973
+Name : Wild Side
+
+TrackId : 2627
+Name : Wild Hearted Son
+
+TrackId : 2633
+Name : Wild Flower
+
+TrackId : 2944
+Name : Wild Honey
+
+'''
+
+    assert output == res
