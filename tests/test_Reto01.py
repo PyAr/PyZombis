a50e6ae9d5a471a1f55398a887a00c226a513ece
sujith-rek <ps335@snu.edu.in>
2023-06-23 20:55:27 +0530

    add e2e tests for quiz reto1_en

--- tests/test_Reto01.py
+++ tests/test_Reto01.py
@@ -106,3 +106,113 @@ def test_r01(page):
         i += 1
     page.hover("#ac_r01_8_stdout >> text=Quedó ahorcado")
     assert page.is_visible("#ac_r01_8_stdout >> text=Quedó ahorcado") == True
+
+@pytest.mark.vcr()
+def test_r01_en(page):
+    page.goto("challenges/Reto01_en.html")
+
+    # ac_r01_1_en
+    page.click("text=Exercise 1")
+    page.click("#ac_r01_1_en >> text=Save & Run")
+
+    # ac_r01_2_en
+    page.click("text=Exercise 2")
+    page.click('pre[role="presentation"]:has-text("​")')
+    page.keyboard.type("correct = incorrect = ''")
+    page.click("#ac_r01_2_en >> text=Save & Run")
+    # Make sure it passed all unit tests
+    page.hover("#ac_r01_2_en >> text=You passed:")
+    assert page.inner_text("#ac_r01_2_en >> text=You passed:") == "You passed: 100.0% of the tests"
+
+    # # ac_r01_3_en
+    # page.click("text=Exercise 3")
+
+    # # request data for the exercise to the .yaml file
+    # res = requests.get("http://universities.hipolabs.com/search")
+    # datos = json.loads(res.text)
+    # countries = [universidad["country"].lower().replace(" ", "") for universidad in datos]
+
+    # page.click("#ac_r01_3_en >> text=api_url =")
+    # page.keyboard.press("ArrowDown")
+    # for i in range(8):
+    #     page.keyboard.press("Control+Shift+ArrowDown")
+    # page.keyboard.press("Backspace")
+
+    # page.keyboard.type(f"cities = {countries[:10]}")
+    
+    # page.click("text=def choose(cities):")
+    # page.keyboard.press("ArrowDown")
+    # page.keyboard.press("Tab")
+    # page.keyboard.type("return choice(cities)")
+
+    # page.click("#ac_r01_3_en >> text=Save & Run")
+
+    # # Make sure it passed all unit tests
+    # page.hover("#ac_r01_3_en >> text=You passed:")
+    # assert page.inner_text("#ac_r01_3_en >> text=You passed:") == "You passed: 100.0% of the tests"
+
+    # ac_r01_4_en
+    page.click("text=Exercise 4")
+    page.click("#ac_r01_4_en >> text=Save & Run")
+
+    # ac_r01_5_en
+    # Deal with prompt box
+    page.click("text=Exercise 5")
+    page.click("text=x = # Start here >> span")
+
+    page.keyboard.press("Control+Shift+End")
+    page.keyboard.press("Backspace")
+    page.keyboard.type("x = input('Guess a letter: ').lower()")
+    page.keyboard.press("Enter")
+    instructions = ["if x not in letters and x not in (digits + punctuation) and len(x) == 1:", "return x"]
+    for i in instructions:
+        page.keyboard.type(i)
+        page.keyboard.press("Enter")
+    # Fill prompt box
+    page.once("dialog", lambda dialog: dialog.accept("f"))
+    page.click("#ac_r01_5_en >> text=Save & Run")
+    # Make sure it passed all unit tests
+    page.hover("#ac_r01_5_en >> text=You passed:")
+    assert page.inner_text("#ac_r01_5_en >> text=You passed:") == "You passed: 100.0% of the tests"
+
+    # # ac_r01_6_en
+    # page.click("text=Exercise 6")
+    # page.wait_for_load_state()
+    # page.click("#ac_r01_6_en >> text=return")
+    # page.keyboard.press("Control+ArrowRight")
+    # page.keyboard.type(" input('jugar de nuevo S/N: ').lower() == 's'")
+    # page.keyboard.press("Enter")
+    # page.click("#ac_r01_6_en >> text=Save & Run")
+    # # Handle dialogs
+    # with page.expect_event("dialog") as prompt1:
+    #     page.once("dialog", lambda dialog: dialog.accept("S"))
+    # with page.expect_event("dialog") as prompt2:
+    #     page.once("dialog", lambda dialog: dialog.accept("n"))
+    # # Ensure it passed all of the tests
+    # page.hover("#ac_r01_6_en >> text=You passed:")
+    # assert page.inner_text("#ac_r01_6_en >> text=You passed:") == "You passed: 100.0% of the tests"
+
+    # ac_r01_7_en
+    page.click("text=Exercise 7")
+    page.click("text=def win(p_aleatoria, letras_adivinadas):")
+    page.keyboard.press("ArrowDown")
+    page.keyboard.press("Tab")
+    page.keyboard.type("return set(p_aleatoria) == set(letras_adivinadas)")
+    page.click("#ac_r01_7_en >> text=Save & Run")
+    # Make sure it passed all unit tests
+    page.hover("#ac_r01_7_en >> text=You passed:")
+    assert page.inner_text("#ac_r01_7_en >> text=You passed:") == "You passed: 100.0% of the tests"
+
+    # # ac_r01_8_en
+    # page.click("text=Exercise 8")
+    # page.click("#ac_r01_8_en >> text=Save & Run")
+    # # Handle dialogs
+    # i = 0
+    # while page.is_hidden("#ac_r01_8_stdout >> text=You were hanged"):
+    #     with page.expect_event("dialog") as prompt:
+    #         page.once("dialog", lambda dialog: dialog.accept(string.ascii_lowercase[i]))
+    #     i += 1
+    # page.hover("#ac_r01_8_en_stdout >> text=You were hanged")
+    # assert page.is_visible("#ac_r01_8_en_stdout >> text=You were hanged") == True
+
+
