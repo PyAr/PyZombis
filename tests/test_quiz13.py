5a76530c578a76c2f6ce56ecb60af43cd39df25c
sujith-rek <ps335@snu.edu.in>
2023-06-23 19:34:19 +0530

    add e2e tests for quiz 13

--- tests/test_quiz13.py
+++ tests/test_quiz13.py
@@ -14,3 +14,17 @@ def test_quiz13_1(page):
 
     page.hover("#q13_1 >> text=You passed:")
     assert page.inner_text("#q13_1 >> text=You passed:") == "You passed: 100.0% of the tests"
+
+def test_quiz13_1_en(page):
+    page.goto("quiz/Quiz13_en.html")
+    page.wait_for_load_state()
+
+    page.click("text=def remove_duplicates(numbers):")
+    page.keyboard.press("ArrowDown")
+    page.keyboard.press("Tab")
+    page.keyboard.type("return sorted(list(set(numbers)))")
+
+    page.click("#q13_1_en >> *css=button >> text=Run")
+
+    page.hover("#q13_1_en >> text=You passed:")
+    assert page.inner_text("#q13_1_en >> text=You passed:") == "You passed: 100.0% of the tests"
