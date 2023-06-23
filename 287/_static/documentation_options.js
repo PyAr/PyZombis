9be0e18ef87dd073b9000ed13568e0a0a5c86b06
sujith-rek <sujith-rek@users.noreply.github.com>
2023-06-23 14:04:46 +0000

    deploy: 839d408d81ffaf682d4f3fa33318010e0282f548

---
+++ 287/_static/documentation_options.js
@@ -0,0 +1,12 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: '7ef24dc97c9d58b16d8015e0d0ae248d34acdfc5-feat/e2etests',
+    LANGUAGE: 'None',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false
+};
