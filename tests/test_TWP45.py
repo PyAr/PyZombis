2314540b01888080dfebb5514e9972f36394b930
sujith-rek <ps335@snu.edu.in>
2023-06-24 00:18:07 +0530

    uncommenting

--- tests/test_TWP45.py
+++ tests/test_TWP45.py
@@ -3,19 +3,19 @@ import requests
 import json
 
 
-# def test_l45_1(page):
-#     page.goto("lectures/TWP45/TWP45_1.html")
-#     # Run the exercise
-#     page.click("#ac_l45_1 >> *css=button >> text=Run")
+def test_l45_1(page):
+    page.goto("lectures/TWP45/TWP45_1.html")
+    # Run the exercise
+    page.click("#ac_l45_1 >> *css=button >> text=Run")
 
-#     # Select the output iframe
-#     iframe = page.query_selector('//*[@id="ac_l45_1"]/div/div[5]/iframe').content_frame()
-#     iframe.fill("#input_usuario", "ACDC")
-#     iframe.click("#boton_mostrar")
-#     img_src = iframe.get_attribute("#img_obtenida", "src")
+    # Select the output iframe
+    iframe = page.query_selector('//*[@id="ac_l45_1"]/div/div[5]/iframe').content_frame()
+    iframe.fill("#input_usuario", "ACDC")
+    iframe.click("#boton_mostrar")
+    img_src = iframe.get_attribute("#img_obtenida", "src")
 
-#     # Test the src attribute from image matches the Facebook URL
-#     assert img_src == "https://graph.facebook.com/ACDC/picture?type=large"
+    # Test the src attribute from image matches the Facebook URL
+    assert img_src == "https://graph.facebook.com/ACDC/picture?type=large"
 
 
 @pytest.mark.vcr()
