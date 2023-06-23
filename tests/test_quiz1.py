b51b8b49c92bd397cce117e97108713ca3b69023
sujith-rek <ps335@snu.edu.in>
2023-06-23 13:27:32 +0530

    feat: add e2e tests for quiz1

--- tests/test_quiz1.py
+++ tests/test_quiz1.py
@@ -18,6 +18,22 @@ def test_quiz1_1(page):
     page.hover("#q1_1 >> text=You passed:")
     assert page.inner_text("#q1_1 >> text=You passed:") == "You passed: 100.0% of the tests"
 
+def test_quiz1_1_en(page):
+    page.goto("quiz/Quiz1_en.html")
+
+    # Do the exercise
+    page.click("text=Exercise 1")
+    page.click("text=def suma(n, m):")
+    page.keyboard.press("ArrowDown")
+    page.keyboard.press("Tab")
+    page.keyboard.type("return n+m")
+
+    # Run the exercise
+    page.click("#q1_1_en >> *css=button >> text=Run")
+
+    # Check it passed all unit tests
+    page.hover("#q1_1_en >> text=You passed:")
+    assert page.inner_text("#q1_1_en >> text=You passed:") == "You passed: 100.0% of the tests"
 
 def test_quiz1_2(page):
     page.goto("quiz/Quiz1.html")
@@ -36,3 +52,21 @@ def test_quiz1_2(page):
     # Check it passed all unit tests
     page.hover("#q1_2 >> text=You passed:")
     assert page.inner_text("#q1_2 >> text=You passed:") == "You passed: 100.0% of the tests"
+
+def test_quiz1_2_en(page):
+    page.goto("quiz/Quiz1_en.html")
+
+    # Do the exercise
+
+    page.click("text=Exercise 2")
+    page.click("text=def metros_a_milimetros(n):")
+    page.keyboard.press("ArrowDown")
+    page.keyboard.press("Tab")
+    page.keyboard.type("return n * 1000")
+
+    # Run the exercise
+    page.click("#q1_2_en >> *css=button >> text=Run")
+
+    # Check it passed all unit tests
+    page.hover("#q1_2_en >> text=You passed:")
+    assert page.inner_text("#q1_2_en >> text=You passed:") == "You passed: 100.0% of the tests"
