5f4542b83330f3f999b5ff786d31a23c285732fd
sujith-rek <ps335@snu.edu.in>
2023-06-23 19:29:24 +0530

    add e2e tests for quiz 10

--- tests/test_quiz10.py
+++ tests/test_quiz10.py
@@ -14,3 +14,19 @@ def test_quiz10_3(page):
 
     page.hover("#q10_3 >> text=You passed:")
     assert page.inner_text("#q10_3 >> text=You passed:") == "You passed: 100.0% of the tests"
+
+def test_quiz10_3_en(page):
+    page.goto("quiz/Quiz10_en.html")
+
+    # Do the exercise
+    page.click("text=Exercise 3")
+    page.click("text=def remplazar_primer_caracter(s):")
+    page.keyboard.press("ArrowDown")
+    page.keyboard.press("Tab")
+    page.keyboard.type("return s[0] + s[1:].replace(s[0], '*')")
+
+    # Run the exercise and check it passed all tests
+    page.click("#q10_3_en >> *css=button >> text=Run")
+
+    page.hover("#q10_3_en >> text=You passed:")
+    assert page.inner_text("#q10_3_en >> text=You passed:") == "You passed: 100.0% of the tests"
