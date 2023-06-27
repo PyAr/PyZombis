0b01992d0dd1a9afbb186f7ea45ea1d11c6aea6c
sujith-rek <sujith-rek@users.noreply.github.com>
2023-06-27 05:05:18 +0000

    deploy: 839d408d81ffaf682d4f3fa33318010e0282f548

--- 286/_static/documentation_options.js
+++ 286/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'ec8337e9f268a4def182ccf16a640d43cfa365fb-feat/babynames',
+    VERSION: 'ba159cfe6da59f40e582543094f057e7f318f80a-feat/babynames',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
