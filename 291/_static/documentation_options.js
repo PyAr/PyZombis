38b8d05603898eabdb905080f4ef7ad5330c0b58
sujith-rek <sujith-rek@users.noreply.github.com>
2023-08-11 06:13:13 +0000

    deploy: 839d408d81ffaf682d4f3fa33318010e0282f548

--- 291/_static/documentation_options.js
+++ 291/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '928dde23a196fef884e6c09cf765552ce907d6de-feat/pygame-lectures',
+    VERSION: 'd39460ba6266ca0dae1bc625347d34fcc4a9362e-feat/pygame-lectures',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
