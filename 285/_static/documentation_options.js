6a7d00fa0c2ee8e41bd61a92382a35e1648fb464
sujith-rek <sujith-rek@users.noreply.github.com>
2023-08-22 19:36:47 +0000

    deploy: 515e2856ba1fd976e9665d6447d9b3d94172e085

--- 285/_static/documentation_options.js
+++ 285/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'dbe184b6dc1b8ee2bcc1558e564f7b165eb8bd78-feat/pymaze',
+    VERSION: '70e1ea76aa8cf260d9dd1b36ee1f27a1ef923fdc-feat/pymaze',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
