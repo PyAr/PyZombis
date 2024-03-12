a5292788faa040bc4a4e4bd5534b354471927a05
its-ChaTTy <its-ChaTTy@users.noreply.github.com>
2024-03-12 21:11:22 +0000

    deploy: c27241b4c009bafcfdf2ca926084a17844645df2

---
+++ 323/_static/documentation_options.js
@@ -0,0 +1,12 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: '0a887f6ae2af84e96418e09e6e09bf43636601f4-Mordern_Exceptions',
+    LANGUAGE: 'None',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false
+};
