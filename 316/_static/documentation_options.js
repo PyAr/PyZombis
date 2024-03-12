2b60054acd9793ed490b11d43a42cef8328ca770
its-ChaTTy <its-ChaTTy@users.noreply.github.com>
2024-03-12 21:39:03 +0000

    deploy: 8d25d31bfb080553446ac1038f1c941f263dc131

---
+++ 316/_static/documentation_options.js
@@ -0,0 +1,12 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: '45c7045f75e725863b8124340e0de65fcf690e8d-linkFix',
+    LANGUAGE: 'None',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false
+};
