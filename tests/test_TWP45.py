396d02f9af7a5444ae383fc6e7670725057f40af
Aayush Badoni <a.badoni@Aayushs-MacBook-Pro-2.local>
2024-03-16 01:34:30 +0530

    added new test for checking data from the facebook url

--- tests/test_TWP45.py
+++ tests/test_TWP45.py
@@ -2,6 +2,16 @@ import pytest
 import requests
 import json
 
+def test_l45_1_api():
+    """Tests fetching data from the provided URL"""
+    # Send a GET request to the URL for a sample user
+    url = "https://graph.facebook.com/arianagrande/picture?type=large"
+
+    # Send a GET request to the URL
+    response = requests.get(url)
+
+    # Check for successful response (status code 200)
+    assert response.status_code == 200, f"Expected status code 200, but got {response.status_code}, Data can not be fetched from the provided URL"
 
 def test_l45_1(page):
     page.goto("lectures/TWP45/TWP45_1.html")
