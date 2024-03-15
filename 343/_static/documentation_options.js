65ce297a13db4b62ef870d83d866977257bdaed5
abadoni5 <abadoni5@users.noreply.github.com>
2024-03-15 21:22:40 +0000

    deploy: 912c7df3fa67a8434a82a1f93ca57c4808de084c

--- 343/_static/documentation_options.js
+++ 343/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2ac2a1709c3b69aa9f59cebdd42e39448aa4dae1-aayush/issue#215',
+    VERSION: '00d467109e3edb86ace69df15bd541f8a68ccebb-aayush/issue#215',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
