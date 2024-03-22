85ec0d28a72f91cc4aa0183b5d371d1118f29328
its-ChaTTy <its-ChaTTy@users.noreply.github.com>
2024-03-22 12:29:58 +0000

    deploy: 912c7df3fa67a8434a82a1f93ca57c4808de084c

---
+++ 354/_static/documentation_options.js
@@ -0,0 +1,12 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: '9771bdc864ca02c048bd1862a430a6c8f39cf60e-loginUI',
+    LANGUAGE: 'None',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false
+};
