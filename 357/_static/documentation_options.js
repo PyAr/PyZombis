ed848f99c5699d9315325e36d997d7572d29f8ea
abadoni5 <abadoni5@users.noreply.github.com>
2024-03-30 12:39:44 +0000

    deploy: 912c7df3fa67a8434a82a1f93ca57c4808de084c

---
+++ 357/_static/documentation_options.js
@@ -0,0 +1,12 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: '68cf776439580af0b47e71322d7d9f6075363f40-aayush/poc_pandas',
+    LANGUAGE: 'None',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false
+};
