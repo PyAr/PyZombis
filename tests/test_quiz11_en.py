e2c74030b3d603ae3eeaf791e0ecc8e2149dc8f4
Iván Nahuel <62953525+ivannrnt@users.noreply.github.com>
2024-05-20 14:04:55 -0400

    create test_quiz11_en.py
    new test created for translate functions names

---
+++ tests/test_quiz11_en.py
@@ -0,0 +1,60 @@
+def test_quiz11_1(page):
+    # Go to http://localhost:8000/quiz/Quiz11_en.html
+    page.goto("quiz/Quiz11.html")
+    page.wait_for_load_state()
+
+    # Do the exercise
+    page.click("text=def verb(s):")
+    page.keyboard.press("ArrowDown")
+    page.keyboard.press("Tab")
+
+    # Code to type
+    ins = ["if len(s) >= 3:", "if s[-3:] != 'ing':",
+           "s = s + 'ing'", "else:", "s = s + 'ly'"]
+    for i in ins[:3]:
+        page.keyboard.type(i)
+        page.keyboard.press("Enter")
+    for i in range(4):
+        page.keyboard.press("Backspace")
+    for i in ins[3:]:
+        page.keyboard.type(i)
+        page.keyboard.press("Enter")
+    for i in range(8):
+        page.keyboard.press("Backspace")
+    page.keyboard.type("return s")
+
+    # Run the exercise and check it passed all unit tests
+    page.click("#q11_1 >> *css=button >> text=Run")
+
+    page.hover("#q11_1 >> text=You passed:")
+    assert page.inner_text(
+        "#q11_1 >> text=You passed:") == "You passed: 100.0% of the tests"
+
+
+def test_quiz11_1_en(page):
+    page.goto("quiz/Quiz11_en.html")
+    page.wait_for_load_state()
+
+    page.click("text=def verb(s):")
+    page.keyboard.press("ArrowDown")
+    page.keyboard.press("Tab")
+
+    ins = ["if len(s) >= 3:", "if s[-3:] != 'ing':",
+           "s = s + 'ing'", "else:", "s = s + 'ly'"]
+    for i in ins[:3]:
+        page.keyboard.type(i)
+        page.keyboard.press("Enter")
+    for i in range(4):
+        page.keyboard.press("Backspace")
+    for i in ins[3:]:
+        page.keyboard.type(i)
+        page.keyboard.press("Enter")
+    for i in range(8):
+        page.keyboard.press("Backspace")
+    page.keyboard.type("return s")
+
+    page.click("#q11_1_en >> *css=button >> text=Run")
+
+    page.hover("#q11_1_en >> text=You passed:")
+    assert page.inner_text(
+        "#q11_1_en >> text=You passed:") == "You passed: 100.0% of the tests"
